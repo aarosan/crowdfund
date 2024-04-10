4162f76eba9ef58f3527ba2abc75325a636a5b8a
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-04-09 20:53:31 -0500

    main page structure set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import Home from './components/pages/HomePage';
+import SignUp from './components/pages/SignUpPage';
+import User from './components/pages/UserPage';
+import FundDetail from './components/pages/FundDetailPage';
+import Search from './components/pages/SearchPage';
+import ErrorPage from './components/pages/ErrorPage';
+import App from './App';
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/user',
+        element: <User />,
+      },
+      {
+        path: '/detail',
+        element: <FundDetail />,
+      },
+      {
+        path: '/search',
+        element: <Search />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
