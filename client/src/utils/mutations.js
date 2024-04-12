f106f8e2f4f6458d9ceb96deb0e322718536337b
RLCastaldy <rlcastaldy@gmail.com>
2024-04-11 20:53:28 -0500

    made file structure changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,16 @@ mutation addUser($username: String!, $email: String!, $password: String!) {
   }
 }
 `;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        email
+        _id
+        username
+      }
+      token
+    }
+  }
+`;
