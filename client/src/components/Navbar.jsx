089b82dc3d02ff4a918c433983edd36c3f7eff63
RLCastaldy <rlcastaldy@gmail.com>
2024-04-15 15:12:07 -0500

    added logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import Logo from "../../public/villageLogo.png"
-
+import Auth from '../utils/auth';
 import '../../src/App.css'
 
 
 
 function Navbar () {
+  const handleLogout = () => {
+    Auth.logout();
+  };
+
     return (
     <>
     <nav className="navbar">
@@ -16,7 +20,11 @@ function Navbar () {
         <li><Link to="/how-it-works" style={{ textDecoration: 'none' }} smooth>How It Works</Link></li>
         <Link to={'/'}><img src={ Logo } /></Link>
         <li><Link to="/join" style={{ textDecoration: 'none' }} smooth>Join The Village</Link></li>
-        <li><Link to="/login" style={{ textDecoration: 'none' }} smooth>Login</Link></li>
+        {Auth.loggedIn() ? (
+            <li><Link to="/" onClick={handleLogout} style={{ textDecoration: 'none' }} smooth>Logout</Link></li>
+          ) : (
+            <li><Link to="/login" style={{ textDecoration: 'none' }} smooth>Login</Link></li>
+          )}
       </ul>
     </nav>
     </>
