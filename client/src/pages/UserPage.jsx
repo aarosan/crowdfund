475d56b72520903b56586eab36c34847d7697818
RLCastaldy <rlcastaldy@gmail.com>
2024-04-16 15:13:06 -0500

    fixing issues with querying users, need to pull Claires changes and test

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,18 +1,27 @@
-// import React from 'react';
-// import Logo from "../../public/villageLogo.png"
-// import { NavLink, Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { GET_ME } from '../utils/queries'
+import Auth from '../utils/auth';
+
 
 import '../../src/App.css'
 
-function User () {
+function User() {
+    const token = Auth.getToken(); // Retrieve the token from Auth (assuming Auth handles token storage)
+    const { loading, error, data } = useQuery(GET_ME, {
+      variables: { token },
+    });
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+  
     return (
-    <>
-    <div className="userCard">
-        <h1 className="welcome">Welcome Back UserName</h1>
-
-    </div>
-    </>
+      <>
+        <div className="userCard">
+          <h1 className="welcome">Welcome Back {data.me.username}</h1>
+          {/* Render other data from the query as needed */}
+        </div>
+      </>
     );
-}
-
-export default User;
+  }
+  
+  export default User;
