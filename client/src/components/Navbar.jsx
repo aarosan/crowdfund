6fd6e99e07a8b20e9b530c4026f6caf0f2ed2aa2
RLCastaldy <rlcastaldy@gmail.com>
2024-04-15 19:11:55 -0500

    fixed all errors in my console

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
+// import React from 'react';
+import { Link } from 'react-router-dom';
+// import { HashLink } from 'react-router-hash-link';
 import Logo from "../../public/villageLogo.png"
 import Auth from '../utils/auth';
 import '../../src/App.css'
@@ -16,14 +16,14 @@ function Navbar () {
     <>
     <nav className="navbar">
       <ul>
-        <li><Link to="/search" style={{ textDecoration: 'none' }} smooth>Search<i class="fa-solid fa-magnifying-glass"></i></Link></li>
-        <li><Link to="/how-it-works" style={{ textDecoration: 'none' }} smooth>How It Works</Link></li>
+        <li><Link to="/search" style={{ textDecoration: 'none' }} >Search<i className="fa-solid fa-magnifying-glass"></i></Link></li>
+        <li><Link to="/how-it-works" style={{ textDecoration: 'none' }} >How It Works</Link></li>
         <Link to={'/'}><img src={ Logo } /></Link>
-        <li><Link to="/join" style={{ textDecoration: 'none' }} smooth>Join The Village</Link></li>
+        <li><Link to="/join" style={{ textDecoration: 'none' }} >Join The Village</Link></li>
         {Auth.loggedIn() ? (
-            <li><Link to="/" onClick={handleLogout} style={{ textDecoration: 'none' }} smooth>Logout</Link></li>
+            <li><Link to="/" onClick={handleLogout} style={{ textDecoration: 'none' }} >Logout</Link></li>
           ) : (
-            <li><Link to="/login" style={{ textDecoration: 'none' }} smooth>Login</Link></li>
+            <li><Link to="/login" style={{ textDecoration: 'none' }} >Login</Link></li>
           )}
       </ul>
     </nav>
