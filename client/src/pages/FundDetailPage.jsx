8c2f99b4cd41610184167d947d7be781e9811435
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2024-04-16 18:38:04 -0500

    worked on linking homepage to detail page

--- client/src/pages/FundDetailPage.jsx
+++ client/src/pages/FundDetailPage.jsx
@@ -1,8 +1,24 @@
-// import React from 'react';
-
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 import "../../src/App.css";
+import { useQuery } from '@apollo/client';
+import { GET_FUND_BY_ID } from '../utils/queries';
 
 function FundDetail () {
+    const { id } = useParams();
+    const { loading, error, data } = useQuery(GET_FUND_BY_ID, {
+        variables: { fundId: id },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :{error.message}</p>;
+
+    const { getFundById: fund } = data;
+
+    if (!fund) return <p>No fund found</p>;
+
+    const totalRaised = fund.donations.reduce((total, donation) => total + donation.amount, 0);
+
     return (
     <>
     <div className="detailCard">
@@ -13,23 +29,25 @@ function FundDetail () {
             </div>
             <div className="actionInfo">
                 <div className="detailTitle">
-                    <h4>Title:</h4>
+                    <h4>Title: {fund.name} </h4>
                 </div>
                 <div className="description">
-                    <p>Description:</p>
+                    <p>Description: {fund.description} </p>
                 </div>
                 <div className="amountDonated">
-                    <h4>Amount Dontated:</h4>
+                    <h4>Amount Dontated:{totalRaised} </h4>
                 </div>
             </div>
         </div>
         <div className="donationDetail">
             <h4 className="donationHeader">Donations Made</h4>
             <div>
-                <div className="donationGiven">
-                    <h4>Given By:</h4>
-                    <h4>Amount:</h4>
-                </div>
+                {fund.donations.map((donation, index)=> (
+                    <div key={index} className="donationGiven">
+                        <h4>Given By:</h4>
+                        <h4>Amount:{donation.amount}</h4>
+                    </div>
+                ))}
             </div>
         </div>
 
