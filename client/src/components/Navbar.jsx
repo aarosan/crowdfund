86e17f34a60d715a62debedabbcd5fc9c008afed
RLCastaldy <rlcastaldy@gmail.com>
2024-04-15 21:10:46 -0500

    fixed homepage and login functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,11 @@ function Navbar () {
         <li><Link to="/search" style={{ textDecoration: 'none' }} >Search<i className="fa-solid fa-magnifying-glass"></i></Link></li>
         <li><Link to="/how-it-works" style={{ textDecoration: 'none' }} >How It Works</Link></li>
         <Link to={'/'}><img src={ Logo } /></Link>
-        <li><Link to="/join" style={{ textDecoration: 'none' }} >Join The Village</Link></li>
+        {Auth.loggedIn() ? (
+            <li><Link to="/user" style={{ textDecoration: 'none' }} >Your Calls To Action</Link></li>
+          ) : (
+            <li><Link to="/join" style={{ textDecoration: 'none' }} >Join The Village</Link></li>
+          )}
         {Auth.loggedIn() ? (
             <li><Link to="/" onClick={handleLogout} style={{ textDecoration: 'none' }} >Logout</Link></li>
           ) : (
