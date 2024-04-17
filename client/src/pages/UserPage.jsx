14b7fc97e26f17237828ad739efd2607ff5493cc
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-04-16 21:52:47 -0500

    UserPage styling and responsivness and navbar fix

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -16,22 +16,19 @@ function User() {
   
     return (
       <>
-        <div className="userCard">
-          <h1 className="welcome">Welcome back {data.me.username}</h1>
-        </div>
-        <div>
-          <h1 className="welcome">Email: {data.me.email}</h1>
-        </div>
-        <div>
+      <div className="userCard">
+          <h1 className="welcome">Welcome back, {data.me.username}</h1>
+          <h1 className="userEmail">Email: {data.me.email}</h1>
+        <div className="myCalls">
           {data.me.funds.map((fund, index)=> (
                     <div key={index} >
-                        <h1>Your Calls:{fund.name}</h1>
-                        <h1>Call Description:{fund.description}</h1>
-                        <h1>Goal:{fund.goal}</h1>
+                        <h1 className="detailTitle">Your Calls:{fund.name}</h1>
+                        <h1 className="description">Call Description:{fund.description}</h1>
+                        <h1 className="amountDonated">Goal:{fund.goal}</h1>
                     </div>
-                    
                 ))}
         </div>
+      </div>
       </>
     );
   }
